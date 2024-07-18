a8d74cb6af5258fe31d36a20f8224edac7fca92a
antonio <antonio.fr.martinezc@hotmail.com>
2024-07-17 20:05:40 -0400

    implemented functionality to get all colors and get one color by id

--- controllers/colorsController.js
+++ controllers/colorsController.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const colors = express.Router();
-const { getAllColors } = require("../queries/color");
+const { getAllColors, getColor, createColor } = require("../queries/color");
 
 colors.get("/", async (req, res) => {
 	const allColors = await getAllColors();
@@ -10,5 +10,22 @@ colors.get("/", async (req, res) => {
 	} else {
 		res.status(500).json({ error: "server error" });
 	}
+});
+
+colors.get("/:id", async (req, res) => {
+	const { id } = req.params;
+	const color = await getColor(id);
+	if (color) {
+		res.status(200).json(color);
+	} else {
+		res.status(404).json({ error: `Color with id: ${id} not found` });
+	}
+});
+
+//CREATE
+
+colors.post("/", async (req, res) => {
+	const newColor = await createColor(req.body);
+	res.status(200).json(newColor);
 });
 module.exports = colors;
--- queries/color.js
+++ queries/color.js
@@ -6,8 +6,29 @@ const getAllColors = async () => {
 
 		return allColors;
 	} catch (error) {
-		return error;
+		throw error;
 	}
 };
 
-module.exports = { getAllColors };
+const getColor = async (id) => {
+	//"SELECT * FROM colors WHERE id = $1",id) $1 is a way to create a variable, pgpromise will place the value of the second argument in that variable
+	try {
+		const oneColor = await db.one("SELECT * FROM colors WHERE id = $1", id);
+		return oneColor;
+	} catch (error) {
+		throw error;
+	}
+};
+
+const createColor = async ({ name, is_favorite }) => {
+	try {
+		const newColor = db.one(
+			"INSERT INTO colors (name, is_favorite) VALUES ($1,$2) RETURNING *",
+			[name, is_favorite]
+		);
+		return newColor;
+	} catch (error) {
+		throw error;
+	}
+};
+module.exports = { getAllColors, getColor, createColor };
